bd76501260bfbb59dd0f8c80c3b56ab0716b7bcd
Tom Maloney <malonetc@mail.c.edu>
2022-07-19 17:10:03 -0400

    Changes to gui.py

--- gui.py
+++ gui.py
@@ -1,50 +1,20 @@
-# import PySimpleGUI as sg
-# from pathlib import Path
-#
-# import subprocess as sp
-#
-# import pdb
-#
-# # def view_workspace(workspace_dir, dsi_studio_path):
-# #     workspace_dir = Path(workspace_dir)
-# #     assert workspace_dir.exists()
-# #     source_dir = workspace_dir.joinpath('slices')
-# #     source_file = list(source_dir.glob('*.nii*'))[0]
-# #     query = [str(dsi_studio_path), '--action=vis', f'--source={str(source_file)}', '--stay_open=1', f'--cmd=load_workspace,{str(workspace_dir)}']
-# #     print(query)
-# #     res = sp.check_output(query)
-# #
-# # if Path('/Applications/dsi_studio.app/Contents/MacOS/dsi_studio').exists():
-# #     dsi_studio_path = Path('/Applications/dsi_studio.app/Contents/MacOS/dsi_studio')
-# # else:
-# #     dsi_studio_path = None
-# #
-# # sg.theme('Dark Blue 3')  # please make your windows colorful
-# #
-# # layout = [[sg.Text('Load info')],
-# #       [sg.Text('DSI_Studio executable', size=(15, 1)), sg.InputText(), sg.FileBrowse(initial_folder=str(dsi_studio_path))]
-# #       [sg.Text('Workspace list', size=(15, 1)), sg.InputText(), sg.FileBrowse()],
-# #       [sg.Submit(), sg.Cancel()]]
-# #
-# # window = sg.Window('Select DSI Studio workspace list', layout)
-# #
-# # event, values = window.read()
-# # window.close()
-# #
-# # pdb.set_trace()
-# # file_path = Path(values[0])      # get the data from the values dictionary
-# # print(file_path)
-
-
+from pathlib import Path
+import sys
 import PySimpleGUI as sg
 import random
 import string
+import pandas as pd
+import subprocess as sp
+import main as dwl
+
+import pdb
 
 """
     Basic use of the Table Element
 """
 
 sg.theme('Dark Blue 3')
+
 
 # ------ Some functions to help generate data for the table ------
 def word():
@@ -59,42 +29,71 @@ def make_table(num_rows, num_cols):
         data[i] = [word(), *[number() for i in range(num_cols - 1)]]
     return data
 
-# ------ Make the Table Data ------
-data = make_table(num_rows=15, num_cols=6)
-headings = [str(data[0][x])+'     ..' for x in range(len(data[0]))]
+def main(file_path):
 
-# ------ Window Layout ------
-layout = [[sg.Table(values=data[1:][:], headings=headings, max_col_width=25,
-                    # background_color='light blue',
-                    auto_size_columns=True,
-                    display_row_numbers=True,
-                    justification='right',
-                    num_rows=20,
-                    alternating_row_color='lightyellow',
-                    key='-TABLE-',
-                    row_height=35,
-                    tooltip='This is a table')],
-          [sg.Button('Read'), sg.Button('Double'), sg.Button('Change Colors')],
-          [sg.Text('Read = read which rows are selected')],
-          [sg.Text('Double = double the amount of data in the table')],
-          [sg.Text('Change Colors = Changes the colors of rows 8 and 9')]]
+    dsi_studio_path = dwl.get_dsi_path()
+    #df = pd.read_csv(file_path, names=['Path'])
+    df = pd.read_csv(file_path)
 
-# ------ Create Window ------
-window = sg.Window('The Table Element', layout,
-                   # font='Helvetica 25',
-                   )
 
-# ------ Event Loop ------
-while True:
-    event, values = window.read()
-    print(event, values)
-    if event == sg.WIN_CLOSED:
-        break
-    if event == 'Double':
-        for i in range(len(data)):
-            data.append(data[i])
-        window['-TABLE-'].update(values=data)
-    elif event == 'Change Colors':
-        window['-TABLE-'].update(row_colors=((8, 'white', 'red'), (9, 'green')))
+    # ------ Make the Table Data ------
+    # # TODO: Change to just take a list of directories and construct the Dataframe from that
+    data = df.to_numpy().tolist()
+    headings = list(df.columns)
+    # data = make_table(num_rows=15, num_cols=6)
+    #headings = [str(data[0][x])+'     ..' for x in range(len(data[0]))]
 
-window.close()
+    # ------ Window Layout ------
+    layout = [[sg.Table(values=data, headings=headings, max_col_width=55,
+                        # background_color='light blue',
+                        auto_size_columns=True,
+                        display_row_numbers=True,
+                        justification='right',
+                        num_rows=df.shape[0],
+                        alternating_row_color='green',
+                        key='-TABLE-',
+                        row_height=35,
+                        tooltip='This is a table')],
+              [sg.Button('Read'), sg.Button('Double'), sg.Button('Open'), sg.Button('Approve')],
+              [sg.Text('Read = read which rows are selected')],
+              [sg.Text('Double = double the amount of data in the table')],
+              [sg.Text('Change Colors = Changes the colors of rows 8 and 9')]]
+
+    #XXX Add export button
+
+    # ------ Create Window ------
+    window = sg.Window('The Table Element', layout,
+                       # font='Helvetica 25',
+                       )
+
+    # ------ Event Loop ------
+    while True:
+        event, values = window.read()
+        print(event, values)
+        if event == sg.WIN_CLOSED:
+            break
+        if event == 'Double':
+            for i in range(len(data)):
+                data.append(data[i])
+            window['-TABLE-'].update(values=data)
+        elif event == 'Open':
+            selected_values = values.get('-TABLE-')
+            selected_value = selected_values[0]
+            print(selected_value)
+            print(data[selected_value])
+            workspace_path = Path(data[selected_value][1])
+            if workspace_path.exists():
+                dwl.view_workspace(workspace_path, dsi_studio_path)
+        elif event == 'Approve':
+            selected_values = values.get('-TABLE-')
+            selected_value = selected_values[0]
+            data[selected_value][3] = 1
+            window['-TABLE-'].update(values=data)
+        # elif event == 'Change Colors':
+        #     window['-TABLE-'].update(row_colors=((8, 'white', 'red'), (9, 'green')))
+
+    window.close()
+
+if __name__ == "__main__":
+    file_path = Path(sys.argv[1])
+    main(file_path)
